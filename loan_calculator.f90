ceec807466c8b576a3acf8f7adbf0a4046f2b0e5
Daniel Solus <dsolus@github.com>
2016-09-30 12:32:04 -0500

    too long since last commit, add payment table, formattable headers, add total payments function, clean up

--- loan_calculator.f90
+++ loan_calculator.f90
@@ -6,8 +6,9 @@ program loan_calculator
     !Define/add variables, integer? array??
     real,dimension(3, 5) :: data
     real,dimension(3) :: loanTerms
-    integer :: i, N, initial_amount, total_years
-    real :: rate, J, payment
+    integer :: i, N, initial_amount, total_years, counter
+    real :: rate, J, payment, interest, balance, principal, total
+
 
     !Prompt the user to enter the name of a file that has the  loan terms.
     !print*, "Input the data file: "
@@ -32,9 +33,9 @@ program loan_calculator
     write(*,*),"loan terms", loanTerms
 
     !seperate loan terms
-    initial_amount = loanTerms(1)
-    total_years = loanTerms(2)
-    rate = loanTerms(3)
+    initial_amount = 170000 !loanTerms(1)
+    total_years = 15 !loanTerms(2)
+    rate = 2.875 !loanTerms(3)
     write(*,*),"Initial Balance: $", initial_amount
     write(*,*),"Years to Pay: ", total_years
     write(*,*),"Interest Rate: ", rate,"%"
@@ -53,7 +54,41 @@ program loan_calculator
     payment = initial_amount * (J / (1 - (1 + J)**(-N)))
     write(*,*),"Monthly Payment: $", payment
     !Calculate the monthly payment schedule (only display first and last years)
+    !We need a while loop to print out the number of periods
+    counter = 1
+    write(*, '(6X, A6, 4X, A9, 9X, A8, 10X, A7)') 'Period', 'Principal', 'Interest', 'Balance' 
+   
+
+    !initialize the balance for the loan loop
+    balance = initial_amount
+    do while (counter < (N + 1))
+        !calculate the interest amount paid
+        interest = balance*rate/(12*100)
+        !calculate the principal amount paid
+        principal = payment - interest
+        
+        balance = (balance + interest) - payment
+        !if (balance < payment) then
+        !    balance = 0.00
+        !endif
+        write(*,*), counter, principal, interest, balance
+        counter = counter + 1
+    enddo
+
     !Calculate Total amount paid
+    total = total_payments(N, payment)
+    write(*,*),'Total Payments: $', total
+
+contains
+    function total_payments(x, y)
+        integer, parameter  :: RP = selected_real_kind(15)
+        integer, intent(IN) :: x
+        Real, intent(IN) :: y
+        Real(kind = RP) :: total_payments
+
+        total_payments = y*x
+    end function total_payments
+
     !Calculate Total interest paid
 endprogram loan_calculator
 
